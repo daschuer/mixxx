25992f1bfc8e7ddce09d049b93bf675b8ffd7b8d
jmigual <j.marce.igual@gmail.com>
2016-05-12 23:04:08 +0200

    Add function to add a WLibraryStack

--- src/widget/wlibraryviewmanager.cpp
+++ src/widget/wlibraryviewmanager.cpp
@@ -1,7 +1,18 @@
 #include "wlibraryviewmanager.h"
 
-WLibraryViewManager::WLibraryViewManager()
-{
+WLibraryViewManager::WLibraryViewManager() {
+
+}
+
+void WLibraryViewManager::addWLibraryStack(WLibraryStack *stack) {
+
+    for (WLibraryStack* s : m_stacks) {
+        // Check not to add the same stack twice
+        if (s == stack) {
+            return;
+        }
+    }
     
+    m_stacks.append(stack);
 }
 
--- src/widget/wlibraryviewmanager.h
+++ src/widget/wlibraryviewmanager.h
@@ -1,11 +1,18 @@
 #ifndef WLIBRARYVIEWMANAGER_H
 #define WLIBRARYVIEWMANAGER_H
 
+#include "wlibrarystack.h"
 
 class WLibraryViewManager
 {
-public:
+  public:
     WLibraryViewManager();
+
+    void addWLibraryStack(WLibraryStack *stack);
+
+  private:
+
+    QList<WLibraryStack*> m_stacks;
 };
 
 #endif // WLIBRARYVIEWMANAGER_H
