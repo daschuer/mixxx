f09cf01e2a161da79e7a14fbcb8cb08bb94c7168
jmigual <j.marce.igual@gmail.com>
2016-05-16 22:30:10 +0200

    Added next and previous functions to librarystack

--- src/widget/wlibrarystack.cpp
+++ src/widget/wlibrarystack.cpp
@@ -1,8 +1,18 @@
 #include "wlibrarystack.h"
 
-WLibraryStack::WLibraryStack(QWidget *parent)
+WLibraryStack::WLibraryStack(QWidget* parent)
         : QStackedWidget(parent),
           WWidget(parent) {
 
+}
+
+void WLibraryStack::showNext() {
+    int newIndex = (currentIndex() + 1) % count();
+    setCurrentIndex(newIndex);
+}
+
+void WLibraryStack::showPrevious() {
+    int newIndex = currentIndex() - 1;
+    setCurrentIndex(newIndex >= 0 ? newIndex : count() - 1);
 }
 
--- src/widget/wlibrarystack.h
+++ src/widget/wlibrarystack.h
@@ -10,6 +10,18 @@ class WLibraryStack : public QStackedWidget, public WWidget
 {
   public:
     WLibraryStack(QWidget *parent = nullptr);
+    
+  public slots:
+    
+    void showNext();
+    
+    void showPrevious();
+    
+  private:
+    
+    WPushButton* m_nextButton;
+    WPushButton* m_previousButton;
+    WPushButton* m_addButton;
 };
 
 #endif // WLIBRARYSTACK_H
