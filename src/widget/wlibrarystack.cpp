dd862449b78dc17afaeffcbbff6218a7c452fd00
jmigual <j.marce.igual@gmail.com>
2016-05-18 21:17:01 +0200

    Add menu options for wlibrarystack

--- src/widget/wlibrarystack.cpp
+++ src/widget/wlibrarystack.cpp
@@ -1,3 +1,4 @@
+#include <QDebug>
 #include "wlibrarystack.h"
 
 
@@ -5,13 +6,44 @@ WLibraryStack::WLibraryStack(QWidget* pParent,
                              ControlObject* pNextControl,
                              ControlObject* pPrevControl,
                              ControlObject* pCurrentPageControl,
-                             ControlObject* pDropDownControl)
+                             ControlObject* pConfigControl)
         : WWidgetStack(pParent,
                        pNextControl,
                        pPrevControl,
                        pCurrentPageControl),
-          m_dropDownControl(
-              pDropDownControl ?
-              pDropDownControl->getKey() : ConfigKey(), this) {
-    
+          m_configControl(
+              pConfigControl ?
+              pConfigControl->getKey() : ConfigKey(), this),
+          m_pActions(ConfigActions::NUM_ACTIONS) {
+
+    m_configControl.connectValueChanged(SLOT(onConfigControlChanged(double)));
+
+
+    m_pMenu = new QMenu(this);
+    m_pMenuAdd = new QMenu(m_pMenu);
+    m_pMenuAdd->setTitle(tr("Add Feature"));
+
+    m_pActions[ConfigActions::LIBRARY] = m_pMenuAdd->addAction(tr("Library"));
+    m_pActions[ConfigActions::LIBRARY_SIDEBAR] =
+        m_pMenuAdd->addAction(tr("Library Sidebar"));
+
+    m_pMenu->addMenu(m_pMenuAdd);
+    m_pActions[ConfigActions::REMOVE] = m_pMenu->addAction(tr("Remove current element"));
+}
+
+void WLibraryStack::onConfigControlChanged(double v) {
+    if (v > 0.0) {
+        QAction* ret = m_pMenu->exec(QCursor::pos());
+
+        int index = m_pActions.indexOf(ret);
+        
+        switch (index) {
+            case ConfigActions::REMOVE:            
+                removeWidget(currentWidget());
+                break;
+                
+            default:
+                qWarning() << "Index" << index << "not found for" << ret;
+        }
+    }
 }
--- src/widget/wlibrarystack.h
+++ src/widget/wlibrarystack.h
@@ -1,23 +1,42 @@
 #ifndef WLIBRARYSTACK_H
 #define WLIBRARYSTACK_H
 
-#include <QStackedWidget>
+#include <QMenu>
 
-#include "wpushbutton.h"
 #include "wwidgetstack.h"
 
 class WLibraryStack : public WWidgetStack
 {
+    Q_OBJECT
+    
+    enum ConfigActions {
+        REMOVE,
+        LIBRARY,
+        LIBRARY_SIDEBAR,
+        
+        // This should be always the last value
+        NUM_ACTIONS
+    };
+
   public:
     WLibraryStack(QWidget* pParent,
                   ControlObject* pNextControl,
                   ControlObject* pPrevControl,
                   ControlObject* pCurrentPageControl,
-                  ControlObject* pDropDownControl);
+                  ControlObject* pConfigControl);
     
+  private slots:
+
+    void onConfigControlChanged(double v);
+
   private:
+
+    ControlProxy m_configControl;
     
-    ControlProxy m_dropDownControl;
+    QMenu* m_pMenu;
+    QMenu* m_pMenuAdd;
+    
+    QVector<QAction*> m_pActions;
 };
 
 #endif // WLIBRARYSTACK_H
