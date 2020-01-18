1a5372a37b87e8d4726d50ed1971e42a7570793d
Daniel Schürmann <daschuer@mixxx.org>
2020-01-18 23:47:05 +0100

    Added shout_idjc to CMakeLists.txt as well

--- cmake/modules/FindShoutidjc.cmake
+++ cmake/modules/FindShoutidjc.cmake
@@ -7,14 +7,14 @@
 FindShout
 ---------
 
-Finds the Shout library.
+Finds the Shoutidjc library.
 
 Imported Targets
 ^^^^^^^^^^^^^^^^
 
 This module provides the following imported targets, if found:
 
-``Shout::Shout``
+``Shoutidjc::Shoutidjc``
   The Shout library
 
 Result Variables
@@ -23,64 +23,72 @@ Result Variables
 This will define the following variables:
 
 ``Shout_FOUND``
-  True if the system has the Shout library.
-``Shout_INCLUDE_DIRS``
-  Include directories needed to use Shout.
-``Shout_LIBRARIES``
-  Libraries needed to link to Shout.
-``Shout_DEFINITIONS``
-  Compile definitions needed to use Shout.
+  True if the system has the Shoutidjc library.
+``Shoutidjc_INCLUDE_DIRS``
+  Include directories needed to use Shoutidjc.
+``Shoutidjc_LIBRARIES``
+  Libraries needed to link to Shoutidjc.
+``Shoutidjc_DEFINITIONS``
+  Compile definitions needed to use Shoutidjc.
+``Shoutidjc_VERSION``
+  The Version of libshout-idjc.
 
 Cache Variables
 ^^^^^^^^^^^^^^^
 
 The following cache variables may also be set:
 
-``Shout_INCLUDE_DIR``
-  The directory containing ``shout/shout.h``.
-``Shout_LIBRARY``
+``Shoutidjc_INCLUDE_DIR``
+  The directory containing ``shoutidjc/shout.h``.
+``Shoutidjc_LIBRARY``
   The path to the Shout library.
 
 #]=======================================================================]
 
 find_package(PkgConfig QUIET)
 if(PkgConfig_FOUND)
-  pkg_check_modules(PC_Shout QUIET shout)
+  pkg_check_modules(PC_Shoutidjc QUIET shout-idjc)
 endif()
 
-find_path(Shout_INCLUDE_DIR
-  NAMES shout/shout.h
-  PATHS ${PC_Shout_INCLUDE_DIRS}
-  DOC "Shout include directory")
-mark_as_advanced(Shout_INCLUDE_DIR)
+set(Shoutidjc_VERSION ${PC_Shoutidjc_VERSION}) 
+mark_as_advanced(Shoutidjc_VERSION)
 
-find_library(Shout_LIBRARY
-  NAMES shout
-  PATHS ${PC_Shout_LIBRARY_DIRS}
-  DOC "Shout library"
+find_path(Shoutidjc_INCLUDE_DIR
+  NAMES shoutidjc/shout.h
+  PATHS ${PC_Shoutidjc_INCLUDE_DIRS}
+  DOC "Shoutidjc include directory")
+mark_as_advanced(Shoutidjc_INCLUDE_DIR)
+
+find_library(Shoutidjc_LIBRARY
+  NAMES shout-idjc
+  PATHS ${PC_Shoutidjc_LIBRARY_DIRS}
+  DOC "Shoutidjc library"
 )
-mark_as_advanced(Shout_LIBRARY)
+mark_as_advanced(Shoutidjc_LIBRARY)
+
+message(STATUS "hallo ${Shoutidjc_LIBRARY}${Shoutidjc_INCLUDE_DIR}") 
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  Shout
-  DEFAULT_MSG
-  Shout_LIBRARY
-  Shout_INCLUDE_DIR
+  Shoutidjc
+  REQUIRED_VARS Shoutidjc_LIBRARY Shoutidjc_INCLUDE_DIR
+  VERSION_VAR Shoutidjc_VERSION
 )
 
-if(Shout_FOUND)
-  set(Shout_LIBRARIES "${Shout_LIBRARY}")
-  set(Shout_INCLUDE_DIRS "${Shout_INCLUDE_DIR}")
-  set(Shout_DEFINITIONS ${PC_Shout_CFLAGS_OTHER})
 
-  if(NOT TARGET Shout::Shout)
-    add_library(Shout::Shout UNKNOWN IMPORTED)
-    set_target_properties(Shout::Shout
+if(Shoutidjc_FOUND)
+  set(Shoutidjc_LIBRARIES "${Shoutidjc_LIBRARY}")
+  set(Shoutidjc_INCLUDE_DIRS "${Shoutidjc_INCLUDE_DIR}")
+  set(Shoutidjc_DEFINITIONS ${PC_Shoutidjc_CFLAGS_OTHER})
+
+  if(NOT TARGET Shoutidjc::Shoutidjc)
+    add_library(Shoutidjc::Shoutidjc UNKNOWN IMPORTED)
+    set_target_properties(Shoutidjc::Shoutidjc
       PROPERTIES
-        IMPORTED_LOCATION "${Shout_LIBRARY}"
-        INTERFACE_COMPILE_OPTIONS "${PC_Shout_CFLAGS_OTHER}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Shout_INCLUDE_DIR}"
+        IMPORTED_LOCATION "${Shoutidjc_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${PC_Shoutidjc_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Shoutidjc_INCLUDE_DIR}"
     )
   endif()
 endif()
+
