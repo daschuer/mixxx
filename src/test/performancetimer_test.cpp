0b97cb4284c871a5a6327af39b12b3efe6e700d3
Daniel Schürmann <daschuer@mixxx.org>
2023-08-29 10:19:38 +0200

    Update performancetimer_test.cpp

--- src/test/performancetimer_test.cpp
+++ src/test/performancetimer_test.cpp
@@ -93,8 +93,8 @@ static void BM_PerformanceTimerNull(benchmark::State& state) {
         // I assume that starting the timer will only start the timer
         // without (unnecessarily) calculating a duration. Also
         // that its valid to call start() multiple times.
-        for (int i; i < 100; ++1) {
-            exti++;
+        for (int i; i < 100; ++i) {
+            exti = exti + 1;
         }
     }
 }
